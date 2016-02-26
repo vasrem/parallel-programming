c51f790c739f0217c723cdb2c281f9054db0a212
vasrem <vasilis@debian>
2016-02-26 16:56:42 +0200

    fixed serial

--- cuda/apsp.c
+++ cuda/apsp.c
@@ -7,23 +7,53 @@
 #define bufSize 1024
 #define INF 999999
 
-float *a;
-int tsize=0;
-
+float *h_a;			// Table at host
+float *d_a;			// Table at device
+int tsize=0;		// number of rows or columns
+size_t size = 0 ;	// size of table( tsize* tsize * sizeof(float*))
 
 
 void print();
 void make_table();
 void run();
 
+
+
 int main(){
 
 	make_table();
-	/*print();*/
+	print();
 	run();
+	
+	
+	// Alloc device table
+	cudaMalloc(&d_a,size);
+
+	// Transfer table to device
+	cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);	
+
+	// Do the math
+	
+
+
+
+
+
+	// Transfer table to host
+
+	cudaMemcpy(h_a, d_a, size, cudaMemcpyDeviceToHost);
+
+	// Free device and host memory
+	
+	cudaFree(d_a);
+	free(h_a);
+
+
 	return 0;
 	
 }
+
+
 /*	run()
  *	-----
  *	Runs Floys Floyd-Warshall's Algorithm
@@ -34,15 +64,16 @@ void run(){
 	for ( i = 0 ; i < tsize ; i++ ){
 		for( j = 0 ; j < tsize ; j++ ){
 			for( k = 0 ; k < tsize ; k++ ){
-				if( a[i*tsize+j] > a[i*tsize+k] + a[k*tsize+j] ){
-					a[i*tsize+j] = a[i*tsize+k] + a[k*tsize+j];
+				if( h_a[i*tsize+j] > h_a[i*tsize+k] + h_a[k*tsize+j] ){
+					h_a[i*tsize+j] = h_a[i*tsize+k] + h_a[k*tsize+j];
 				}				
 			}
 		}
 	}
 	printf("\nDone Serial.\n");
 }
- 
+
+
 /*	make_table()
  *	------------
  *	Gets the input table at table A
@@ -60,14 +91,14 @@ void make_table(){
 	// Read size of table
 	fgets(buf,sizeof(buf),fp);
 	tsize =(int) atof(buf);
-	
-	// Alloc the table
-	a =malloc (tsize*tsize*sizeof(float *));
+	size = tsize*tsize*sizeof(float);	
+	// Alloc the table at host
+	h_a =(float *) malloc (size);
 	
 	// Fill the table
 	while(fgets(buf,sizeof(buf),fp)!=NULL){
 		for(j = 0 ; j < tsize ; j++ ){
-			a[i*tsize+j]=atof(&buf[16*j]);
+			h_a[i*tsize+j]=atof(&buf[16*j]);
 		}
 		i++;
 	}
@@ -75,6 +106,9 @@ void make_table(){
 	fclose(fp);
 	printf("\nDone making table.\n");
 }
+
+
+
 /*	print()
  *	-------
  *	Prints the table A	
@@ -84,10 +118,10 @@ void print(){
 	int j = 0;
 	for(i=0;i<tsize;i++){
 		for(j=0;j<tsize;j++){
-			if(isinf(a[i*tsize+j])){
-				printf("%f\t\t",a[i*tsize+j]);
+			if(isinf(h_a[i*tsize+j])){
+				printf("%f\t\t",h_a[i*tsize+j]);
 			}else{
-				printf("%f\t",a[i*tsize+j]);
+				printf("%f\t",h_a[i*tsize+j]);
 			}
 		}
 		printf("\n-------------------------\n");
