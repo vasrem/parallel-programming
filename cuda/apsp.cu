e7772c085cc20adf9df46cbcc358a7c2b21b9e63
vasrem <vasilis@debian>
2016-02-27 16:07:38 +0200

    kernel 2 rdy

--- cuda/apsp.cu
+++ cuda/apsp.cu
@@ -37,34 +37,48 @@ __global__ void Kernel2(float *A,int N,int k){
 	int j = blockDim.y * blockIdx.y + threadIdx.y;
 
 	__shared__ float k_k1,k1_k;
-
+	/*	example:
+	 *	if we have to from D -> F throw k and then k+1 we have to do:
+	 *	DkF check
+	 *  D(k+1)F check
+	 *	Dk(k+1)F check
+	 *	D(k+1)kF check
+	 *	the min of these is the min dist.
+	 */
 	if(threadIdx.x==0 && threadIdx.y==0){
 		k_k1=A[k*N+(k+1)];
 		k1_k=A[(k+1)*N+k];
 	}
 	float x,y,asked,xn,yn;
 
-	x=A[k*N+j];
-	y=A[i*N+k];
 	asked=A[i*N+j];
 	
+	x=A[k*N+j];
+	y=A[i*N+k];
+
+	// DkF
 	if(asked>x+y){
 		asked=x+y;
 	}
+
 	xn=A[i*N+(k+1)];
 	yn=A[(k+1)*N+j];
+
 	__syncthreads();
 	
+	//	D(k+1)
 	if(xn>y+k_k1){
 		xn=y+k_k1;
 	}
+	//	(k+1)F
 	if(yn>x+k1_k){
 		yn=x+k1_k;
 	}
+	//	D(k+1)F or D(k+1)kF or Dk(k+1)F
 	if(asked>xn+yn){
 		asked=xn+yn;
 	}
-
+	//	min dist
 	A[i*N+j]=asked;
 }
 
